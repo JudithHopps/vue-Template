c2ae0101bbeafc1c960ab8c18fe24403dd591c6b
JudithHopps <113181934+JudithHopps@users.noreply.github.com>
2022-12-27 17:21:33 +0900

    유수빈 - 헤더  active-class 추가

--- src/components/layout/HeaderComponent.vue
+++ src/components/layout/HeaderComponent.vue
@@ -1,21 +1,17 @@
 <template>
-  <nav class="navbar navbar-expand-lg header " style="background-color: #ff7f00;">
+  <nav class="navbar navbar-expand-lg header ">
 <div class="container-fluid">
-  <!-- <a class="navbar-brand ms-5" href="#" style="color:white; font-weight: bold;"> Raon</a> -->
-  <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button> -->
 
-  <div class="d-flex flex-row-reverse collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="d-flex ms-5" id="navbarSupportedContent">
     <ul class="navbar-nav me-5 mb-lg-0">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="/" style="color:white;">Home</a>
+        <router-link class="nav-link" active-class="active" to="/" exact><a>Home</a></router-link>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/intro" style="color:white;">intro</a>
+        <router-link class="nav-link" active-class="active" to="/Portfolio" ><a>Portfolio</a></router-link>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/article" style="color:white;">article</a>
+        <router-link class="nav-link" active-class="active" to="/skill" ><a>Skill</a></router-link>
       </li>
       
     </ul>
